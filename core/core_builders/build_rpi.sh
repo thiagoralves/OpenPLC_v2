a87e31a8e093a9114b52084b75639527a4223375
Thiago Alves <thiagoralves@gmail.com>
2016-08-01 16:12:18 -0500

    added -fpermissive to RasberryPi compile script

--- core/core_builders/build_rpi.sh
+++ core/core_builders/build_rpi.sh
@@ -6,5 +6,5 @@ g++ -I ./lib -c Res0.c
 echo Generating glueVars.cpp
 ./glue_generator
 echo Compiling main program
-g++ *.cpp *.o -o openplc -I ./lib -lrt -lwiringPi -lpthread
+g++ *.cpp *.o -o openplc -I ./lib -lrt -lwiringPi -lpthread -fpermissive
 cd ..
